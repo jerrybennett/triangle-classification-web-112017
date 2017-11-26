d513d7f674b9d2df42c04db8b0eb34b232016cdb
jerrybennett <jerrysbusiness@gmail.com>
2017-11-25 21:20:55 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+
+  def initialize(a, b, c)
+    @sides = [a, b, c].sort
+  end
+
+  def kind
+    if @sides.any? {|i| i <= 0} || @sides[0] + @sides[1] <= @sides[2]
+      # begin
+        raise TriangleError
+      # rescue TriangleError => error
+      #   puts error.message
+      # end
+    elsif @sides[0] == @sides[2]
+      :equilateral
+    elsif @sides[0] == @sides[1] || @sides[1] == @sides[2]
+      :isosceles
+    else
+      :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
+  def message
+    "The length of one or more of the sides is invalid. Please try again."
+  end
 end
